11e69ec54f6d1bf1140c0657f205329bea4aed18
Ankush Kaundal <ankushkaundal3@gmail.com>
2022-09-09 17:41:35 +0530

    Connection to mongo atlas

--- src/main/java/com/example/mongos/MongosApplication.java
+++ src/main/java/com/example/mongos/MongosApplication.java
@@ -1,11 +1,14 @@
 package com.example.mongos;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories("com.example.mongos.repository")
+
 public class MongosApplication {
 
 	public static void main(String[] args) {
@@ -13,4 +16,3 @@ public class MongosApplication {
 	}
 
 }
-	
